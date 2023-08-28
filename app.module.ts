ac068c96bb7dc3659ad5fe3100be73182f94b2ff
Daviddffino <129997879+Daviddffino@users.noreply.github.com>
2023-08-27 21:36:28 -0500

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,12 @@
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    // ...
+  ],
+  imports: [
+    RouterModule.forRoot(routes), // Asegúrate de importar tus rutas aquí
+    // ...
+  ],
+  // ...
+})
